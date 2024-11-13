d142e224fc0d7f2fba0b8698cbc5deeed84b2968
slesmes <santiagolesmesmarin@hotmail.com>
2024-11-12 23:08:20 -0500

    subiendo endopints de brand

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from src.config.database import Base, engine
 
 
 from src.routers.country import country_router
+from src.routers.brand import Brand_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -22,10 +23,15 @@ tags_metadata = [
         "name": "country",
         "description": "country handling endpoints",
     },
+    {
+        "name": "Brand",
+        "description": "brand handling endpoints",
+    },
 ]
 app = FastAPI(openapi_tags=tags_metadata)
 
 app.include_router(prefix="/api/v1/country", router=country_router)
+app.include_router(prefix="/api/v1/brand", router=Brand_router)
 
 app.title = "AUTOLUX"
 app.summary = "API with FastAPI and Python"
